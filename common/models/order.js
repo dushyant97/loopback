e25f167fa50084b47b474f8ec54c000691f6fb59
Dushyant Sharma <dushyant.sharma1997@gmail.com>
2019-06-25 14:04:12 +0530

    done

--- common/models/order.js
+++ common/models/order.js
@@ -1,5 +1,56 @@
 'use strict';
 
 module.exports = function(Order) {
+  
+    
+/**
+ * prints message
+ * @param {string} msg enter the message
+ * @param {Function(Error)} callback
+ */
 
+Order.greet = function(msg, callback) {
+    
+    if (msg =""){
+        
+        var a = {
+        message  :   `String is empty`,
+        StatusCode : "900"
+         };
+        callback(a);
+    }
+
+    var result ={
+        status: `The message is : ${msg}`
+    }
+
+    callback(null,result);
+  };
+
+  Order.operation = function(a, cb) {
+      console.log("test"+a+"test");
+    if (a ==undefined){
+        cb(null,"para not found");
+        // null has to be passed because err can onluy be passed when there is a error
+        // otherwise for user created errors null has to be passed
+    }
+    else{
+        //cb(null, 'Greetings... ' + a);
+        Order.app.models.Employee.find({"where" : {"name" : a}},function(err,re){
+            if(err){
+                console.log(err);
+                return cb(err);
+            }
+            else{
+                cb(null,re);
+                }
+            });
+        }
+     }
+
+  Order.remoteMethod('operation', {
+        accepts: {arg: 'a', type: 'string'},
+        returns: {arg: 'b', type: 'string'}
+  });
+  
 };
